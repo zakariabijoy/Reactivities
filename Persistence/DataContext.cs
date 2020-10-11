09dcbeb3d6eaab97ceea04fa384d0b22b31a12a9
zakariabijoy <zakaria.bijoy@live.com>
2020-10-12 03:40:53 +0600

    Creating the dbcontext and service - Api

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
