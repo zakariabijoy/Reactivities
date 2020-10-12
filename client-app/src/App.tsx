f512130ca04f53e69784a79f3bcdad4967487a87
zakariabijoy <zakaria.bijoy@live.com>
2020-10-13 03:13:59 +0600

    using react state - Client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,25 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount(){
+    this.setState({
+      values: [{id:1, name: 'value 101'}, {id:2, name: 'value 102'}]
+    });
+  }
+
   render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+   return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <ul>
+   {this.state.values.map((value: any) => <li>{value.name}</li>)}
+        </ul>
+      </header>
+    </div>
+  );
   }
  
 }
