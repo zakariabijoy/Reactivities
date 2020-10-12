c6e2b766ad22a3863570dfe4253c8edb0d1cc594
zakariabijoy <zakaria.bijoy@live.com>
2020-10-13 03:24:42 +0600

    fetching data from api using axios http client package - Client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
   state = {
@@ -8,9 +9,15 @@ class App extends Component {
   };
 
   componentDidMount(){
-    this.setState({
-      values: [{id:1, name: 'value 101'}, {id:2, name: 'value 102'}]
-    });
+    axios.get("https://localhost:5001/api/values")
+    .then((response) => {
+      console.log(response);
+      
+      this.setState({
+        values: response.data
+      });
+    })
+    
   }
 
   render(){
