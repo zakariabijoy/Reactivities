817abf335e7903e244d9ff0a4563a15093737f28
zakariabijoy <zakaria.bijoy@live.com>
2020-10-13 01:12:32 +0600

    Creating the  Database and do all migration (if they are not done before) when we run our api  - API

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +16,25 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (System.Exception ex)
+                {
+                    
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error Occured during migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
