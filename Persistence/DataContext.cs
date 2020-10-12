9be9fc3cb3df2b9c918f81b8f239defdd25e3ba7
zakariabijoy <zakaria.bijoy@live.com>
2020-10-13 01:22:48 +0600

    Sedding data using entity framework fluent configuration -  API

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,14 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id= 1, Name="value 101"},
+                new Value { Id= 2, Name="value 103"},
+                new Value { Id= 3, Name="value 102"}
+            );
+        }
     }
 }
