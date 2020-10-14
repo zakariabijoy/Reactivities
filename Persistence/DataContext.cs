c2f99d4f28ab26de5d118f41890901490886cca0
zakariabijoy <zakaria.bijoy@live.com>
2020-10-15 01:56:56 +0600

    Adding the Activity entity - API

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,7 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder){
             builder.Entity<Value>()
