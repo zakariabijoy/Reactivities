caa32d680bd70835e446db6e9bf4414549531c5a
zakariabijoy <zakaria.bijoy@live.com>
2020-10-18 00:42:30 +0600

    Refactoring our class component to functional component with react Hooks - Client

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
 import axios from "axios";
-import {IActivity} from '../models/activity'
+import { IActivity } from "../models/activity";
 
-interface IState{
-  activities : IActivity[]
-}
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: [],
-  };
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-  componentDidMount() {
-    axios.get<IActivity[]>("https://localhost:5001/api/activities").then((response) => {
-
-      this.setState({
-        activities: response.data,
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("https://localhost:5001/api/activities")
+      .then((response) => {
+        setActivities(response.data);
       });
-    });
-  }
+  },[]);
 
-  render() {
-    return (
-      <div >
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-             <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
