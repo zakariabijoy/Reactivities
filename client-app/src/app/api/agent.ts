32350d0197c6a16a5a9e60605c50340db05bf48d
zakariabijoy <zakaria.bijoy@live.com>
2020-10-22 02:27:48 +0600

    adding a delay to our api call - Client

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,18 +1,23 @@
 import axios, { AxiosResponse } from "axios";
+import { resolve } from "dns";
 import { IActivity } from "../models/activity";
 
 axios.defaults.baseURL = "https://localhost:5001/api";
 
 const responseBody = (response: AxiosResponse) => response.data;
+const sleep = (ms: number) => (response: AxiosResponse) =>
+  new Promise<AxiosResponse>((resolve) =>
+    setTimeout(() => resolve(response), ms)
+  );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  del: (url: string) => axios.delete(url).then(responseBody),
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
 };
 const activities = {
-  list: () :Promise<IActivity[]> => requests.get("/activities"),
+  list: (): Promise<IActivity[]> => requests.get("/activities"),
   details: (id: string) => requests.get(`/activities/${id}`),
   create: (activity: IActivity) => requests.post("/activities", activity),
   update: (activity: IActivity) =>
