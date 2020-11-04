ffbeeeb6bce82941364ef1c3fcc7569e074a475b
zakariabijoy <zakaria.bijoy@live.com>
2020-11-05 02:42:30 +0600

    setting up our Routes -Client

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from "./LoadingComponent";
 import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { Route } from "react-router-dom";
+import HomePage from "./../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -20,7 +23,9 @@ const App = () => {
     <React.Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </React.Fragment>
   );
