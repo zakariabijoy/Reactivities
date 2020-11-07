22cca40958b01dbebbdeaf13d7884e38f7654a35
zakariabijoy <zakaria.bijoy@live.com>
2020-11-08 03:28:03 +0600

    Adding the details link - Client

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import { observer } from "mobx-react-lite";
 import { Route } from "react-router-dom";
 import HomePage from "./../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -25,6 +26,7 @@ const App = () => {
       <Container style={{ marginTop: "7em" }}>
         <Route exact path="/" component={HomePage} />
         <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/activities/:id" component={ActivityDetails} />
         <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </React.Fragment>
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../../app/stores/activityStore";
+import { Link } from "react-router-dom";
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
@@ -28,9 +29,8 @@ const ActivityList: React.FC = () => {
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => {
-                    selectActivity(activity.id);
-                  }}
+                  as={Link}
+                  to={`/activities/${activity.id}`}
                   floated="right"
                   content="view"
                   color="blue"
