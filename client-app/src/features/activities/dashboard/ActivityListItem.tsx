d6b72d0dc109d6a7798ea9fee90d804250d9fe53
zakariabijoy <zakaria@vonome.com>
2020-11-24 01:32:00 +0600

    styling activity list -client

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Item, Label } from "semantic-ui-react";
+import { IActivity } from "./../../../app/models/activity";
+
+const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  return (
+    <Item key={activity.id}>
+      <Item.Content>
+        <Item.Header as="a">{activity.title}</Item.Header>
+        <Item.Meta>{activity.date}</Item.Meta>
+        <Item.Description>
+          <div>{activity.description}</div>
+          <div>
+            {activity.city}, {activity.venue}
+          </div>
+        </Item.Description>
+        <Item.Extra>
+          <Button
+            as={Link}
+            to={`/activities/${activity.id}`}
+            floated="right"
+            content="view"
+            color="blue"
+          />
+          <Label basic content={activity.category} />
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+};
+
+export default ActivityListItem;
