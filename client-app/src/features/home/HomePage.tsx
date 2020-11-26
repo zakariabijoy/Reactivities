135752eb06aed57339d7dcb75b079fc4cea9bfd0
zakariabijoy <zakaria@vonome.com>
2020-11-27 02:40:38 +0600

    styling the home page -client

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <Container style={{ marginTop: "7em" }}>
-      <h1>Home Page</h1>
-      <h3>
-        Go To <Link to="/activities">Activities</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        <Header as="h2" inverted content="Welcome to Reactivities" />
+        <Button as={Link} to="/activities" size="huge" inverted>
+          Take me to the activities!
+        </Button>
+      </Container>
+    </Segment>
   );
 }
 
