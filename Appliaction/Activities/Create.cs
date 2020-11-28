816a5a9f4f9c744ec9948451b5a1d87b59ac662a
zakariabijoy <zakaria@vonome.com>
2020-11-29 02:32:32 +0600

    adding validation in api using Data Annotation Attribute -Api

--- Appliaction/Activities/Create.cs
+++ Appliaction/Activities/Create.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
@@ -13,6 +14,8 @@ namespace Appliaction.Activities
         {
 
             public Guid Id { get; set; }
+
+            [Required]
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
