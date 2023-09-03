b7d81e1bc4586893357cda5ca9197289d6ed60f2
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-03 18:33:02 +0600

    Walking Skeleton -API: Property Name Fix of Activity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -8,5 +8,5 @@ public class Activity
     public string Description { get; set; }
     public string Category { get; set; }
     public string City { get; set; }
-    public string Vanue { get; set; }
+    public string Venue { get; set; }
 }
---
+++ Persistence/Migrations/20230903123050_PropertyNameFixOfActivities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyNameFixOfActivities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Vanue",
+                table: "Activities",
+                newName: "Venue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activities",
+                newName: "Vanue");
+        }
+    }
+}
