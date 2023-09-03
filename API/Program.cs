5e61c9285cbaa6054fe89528a8036a59b0b3856d
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-03 13:55:07 +0600

    Walking Skeleton -API: Reviewing the project file

--- API/Program.cs
+++ API/Program.cs
@@ -10,13 +10,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
