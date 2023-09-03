80d37e91f81e4b00843ae691e1d67600cef8a1a2
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-04 01:40:21 +0600

    Walking Skeleton -Client: Fethcing data from the api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() =>{
+    axios.get('http://localhost:5010/api/activities')
+      .then(res => {
+        setActivities(res.data);
+      })
+  }, []);
+
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <h1>Reactivities</h1>
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
