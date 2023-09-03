20109d3294807b6535cbf70acd90664d65005f40
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-03 19:38:16 +0600

    Walking Skeleton -API: Adding ActivitiesController

---
+++ API/Controllers/ActivitesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API;
+
+public class ActivitiesController : BaseApiController
+{
+        private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+            _context = context;
+    }
+
+    [HttpGet] //api/activities
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return Ok(await _context.Activities.ToListAsync());
+    }
+
+    [HttpGet("{id}")] //api/activities/id
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return Ok(await _context.Activities.FindAsync(id)); 
+    }
+}
