8aa54fb22085bc046d49df4cfbde0da10acab915
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-04 02:01:22 +0600

    Walking Skeleton -Client: Semantic UI React

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -14,14 +15,14 @@ function App() {
 
   return (
     <div>
-      <h1>Reactivities</h1>
-      <ul>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>
+          <List.Item key={activity.id}>
             {activity.title}
-          </li>
+          </List.Item>
         ))}
-      </ul>
+      </List>
     </div>
   )
 }
