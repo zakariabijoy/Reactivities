6264474b14c9244fa3bde6458953f50d5d895749
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-03 18:24:53 +0600

    Walking Skeleton -API: Creating the database

--- API/Program.cs
+++ API/Program.cs
@@ -27,4 +27,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services =scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "An error occured during migration");
+}
+
 app.Run();
