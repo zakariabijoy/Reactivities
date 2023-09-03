7fbcbe5e3892c4f90a9206aae5c4a660f3efb558
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-03 17:31:18 +0600

    Walking Skeleton -API: Adding an Entity Framework Db Cotext

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Activity> Activities { get; set; }
+}
