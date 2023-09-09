3322cfe6ea288249b119538fa8cca0d733682a71
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-10 04:08:20 +0600

    Creating a CRUD application using CQRS + Mediator: Creating our first Query handler

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -1,28 +1,28 @@
 ﻿using System.Diagnostics;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API;
 
 public class ActivitiesController : BaseApiController
 {
-        private readonly DataContext _context;
+    private readonly IMediator _mediator;
 
-    public ActivitiesController(DataContext context)
+    public ActivitiesController(IMediator mediator)
     {
-            _context = context;
+        _mediator = mediator;
     }
 
     [HttpGet] //api/activities
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return Ok(await _context.Activities.ToListAsync());
+        return Ok(await _mediator.Send(new List.Query()));
     }
 
     [HttpGet("{id}")] //api/activities/id
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return Ok(await _context.Activities.FindAsync(id)); 
+        return Ok(); 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -19,6 +20,8 @@ builder.Services.AddCors(opt =>{
     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
   });
 });
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
 
 var app = builder.Build();
 
