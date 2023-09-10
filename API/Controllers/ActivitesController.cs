1891887b8677ec663a243897471452829f52bd20
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-10 12:20:51 +0600

    Creating a CRUD application using CQRS + Mediator: Thin controllers in the API

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -7,17 +7,11 @@ namespace API;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator;
-
-    public ActivitiesController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
 
     [HttpGet] //api/activities
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return Ok(await _mediator.Send(new List.Query()));
+        return Ok(await Mediator.Send(new List.Query()));
     }
 
     [HttpGet("{id}")] //api/activities/id
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
@@ -6,5 +7,7 @@ namespace API;
 [Route("api/[controller]")]
 public class BaseApiController : ControllerBase
 {
+    private IMediator _mediator;
 
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 }
