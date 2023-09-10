21253838ec3a65d7e6306cc2cc8a07a177bb6f04
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-10 16:56:15 +0600

    Creating a CRUD application in React: Adding an Activity interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() =>{
-    axios.get('http://localhost:5010/api/activities')
+    axios.get<Activity[]>('http://localhost:5010/api/activities')
       .then(res => {
         setActivities(res.data);
       })
@@ -16,7 +17,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivities'/>
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>
             {activity.title}
           </List.Item>
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+    id: string
+    title: string
+    date: string
+    description: string
+    category: string
+    city: string
+    venue: string
+  }
+  
