4fb5b05751828dab12009cfcc78272f0748295f5
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-10 15:17:58 +0600

    Creating a CRUD application in React: Folder structure in React

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import './app/layout/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
