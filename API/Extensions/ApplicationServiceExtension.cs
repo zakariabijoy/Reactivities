860433052fc51c1e157a3872b6ec9308568cc4a5
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-10 14:17:44 +0600

    Creating a CRUD application using CQRS + Mediator: Clean Up Program Class using extension method

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,31 @@
+using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<DataContext>(opt => {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors(opt =>{
+        opt.AddPolicy("CorsPolicy", policy =>{
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+        });
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
-using Application.Activities;
-using Application.Core;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,22 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(opt =>{
-  opt.AddPolicy("CorsPolicy", policy =>{
-    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-  });
-});
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
