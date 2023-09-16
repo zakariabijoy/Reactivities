95015b16f650809fc9c73c439042db791766bbf4
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-16 18:57:45 +0600

    Mobx: MobX strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,17 +14,23 @@ export default class ActivityStore{
     }
 
     loadActivities = async () =>{
-        this.loadingInitial = true;
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity =>{
                 activity.date = activity.date.split('T')[0];
                 this.activities.push(activity);
-              })
-              this.loadingInitial = false;
+            })
+
+            this.setLoadingInitial(false);
+            
         } catch (error) {
             console.log(error);
-            this.loadingInitial = false;
+            this.setLoadingInitial(false);
         }
+    }
+
+    setLoadingInitial = (state: boolean) =>{
+        this.loadingInitial = state;
     }
 }
