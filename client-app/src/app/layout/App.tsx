d75e73aaecc1394dbcc30057632dfc413c0f4864
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-16 18:27:24 +0600

    Mobx: Refactoring the app to use Mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState } from 'react'
-import { Button, Container} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboad/ActivityDashboard';
@@ -15,20 +15,11 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() =>{
-    agent.Activities.list().then(res => {
-        let activities: Activity[] = [];
-        res.forEach(activity =>{
-          activity.date = activity.date.split('T')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
-        setLoading(false);
-      })
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
   function handleSelectActivity(id:string){
     setSelectedActivity(activities.find(x => x.id === id));
@@ -77,16 +68,14 @@ function App() {
     })
   }
 
-  if(loading) return <LoadingComponent content='Loading app'/>
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
       <NavBar openForm={handleFormOpen}/>
       <Container style={{marginTop: '7em'}}>
-        <h2>{activityStore.title}</h2>
-        <Button content='Add exclamation' positive onClick={activityStore.setTitle}/>
         <ActivityDashboard 
-        activities={activities}
+        activities={activityStore.activities}
         selectedActivity ={selectedActivity}
         selectActivity={handleSelectActivity}
         cancelSelectActivity={handleCancelSelectActivity}
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,14 +1,30 @@
 import { makeAutoObservable} from "mobx"
+import { Activity } from "../models/activity"
+import agent from "../api/agent";
 
 export default class ActivityStore{
-    title= 'Hello From Mobx!!!'
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
-
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';    
+    loadActivities = async () =>{
+        this.loadingInitial = true;
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(activity =>{
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+              })
+              this.loadingInitial = false;
+        } catch (error) {
+            console.log(error);
+            this.loadingInitial = false;
+        }
     }
 }
