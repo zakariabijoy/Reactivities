4ebfc4d3ad49c834196bdaa9af9497e16b0694c6
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-16 13:04:07 +0600

    Creating a CRUD application in React: Using a GUID for the activity id

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboad/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -36,7 +37,7 @@ function App() {
 
   function handleCreateOrEditActivity(activity: Activity){
     activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
-    : setActivities([...activities, activity]);
+    : setActivities([...activities, {...activity, id: uuid()}]);
 
     setEditMode(false);
     setSelectedActivity(activity);
