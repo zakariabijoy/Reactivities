349cc0bf239b0f50df9345a35cc1e077e7db3b68
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 19:54:11 +0600

    Routing: Adding a key to the route

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -13,8 +13,8 @@ export const routes: RouteObject[] =[
             { path:'', element: <HomePage/>},
             { path:'activities', element: <ActivityDashboard/>},
             { path:'activities/:id', element: <ActivityDetails/>},
-            { path:'createActivity', element: <ActivityForm/>},
-            { path:'manage/:id', element: <ActivityForm/>}
+            { path:'createActivity', element: <ActivityForm key='create'/>},  // have to use key if we want to use same component for diffrent route to reset state
+            { path:'manage/:id', element: <ActivityForm key='manage'/>}
         ]
     },
 ]
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore{
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
