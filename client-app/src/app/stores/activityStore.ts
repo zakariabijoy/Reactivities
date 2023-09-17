dc4d34271be2fc2b3ae96d75858e8debd3fbd684
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 20:05:34 +0600

    Routing: Redirect After Submission

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction} from "mobx"
 import { Activity } from "../models/activity"
 import agent from "../api/agent";
-import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore{
     activityRegistry = new Map<string, Activity>();
@@ -70,7 +69,6 @@ export default class ActivityStore{
 
     createActivity = async (activity: Activity) =>{
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity);
             runInAction(() =>{
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,9 +2,10 @@ import { Button, Form, Segment } from "semantic-ui-react";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {v4 as uuid} from 'uuid';
 
 
 export default observer(function ActivityForm(){
@@ -12,6 +13,7 @@ export default observer(function ActivityForm(){
     const {activityStore} = useStore();
     const {selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const [activity, setActivity] = useState<Activity>({
         id: '',
@@ -28,7 +30,12 @@ export default observer(function ActivityForm(){
     },[id, loadActivity]);
     
     function handleSubmit(){
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if(!activity.id){
+            activity.id = uuid();
+            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }else{
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
