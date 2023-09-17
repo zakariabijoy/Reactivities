2ddcdc6bc1e18c8f24ca41d8c76f25ff17f0baf3
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 17:36:42 +0600

    Routing: Adding nav links

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,18 @@
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
+import { NavLink } from "react-router-dom";
 
 
 export default function NavBar(){
-
-    const {activityStore} = useStore();
-
     return(
         <Menu inverted fixed="top">
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name="Activities"/>
+                <Menu.Item  as={NavLink} to='/activities' name="Activities"/>
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm()} positive content='Create Activity'/>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
