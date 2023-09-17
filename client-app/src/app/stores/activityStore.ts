17c5d5e8c138709b2a7c90e02e00676daae9e90e
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-18 03:20:38 +0600

    Styling the UI: Grouping the activities by date

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -18,6 +18,15 @@ export default class ActivityStore{
             Date.parse(a.date) - Date.parse(b.date));
     }
 
+    get groupedAcivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) =>{
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            },{} as {[key:string] : Activity[]})
+        )
+    }
     loadActivities = async () =>{
         this.setLoadingInitial(true);
         try {
--- client-app/src/features/activities/dashboad/ActivityList.tsx
+++ client-app/src/features/activities/dashboad/ActivityList.tsx
@@ -1,20 +1,31 @@
-import { Item, Segment } from "semantic-ui-react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react";
 
 
 export default observer(function ActivityList() {
-    const {activityStore} = useStore();
-    const {activitiesByDate} = activityStore;
-    
+    const { activityStore } = useStore();
+    const { groupedAcivities } = activityStore;
+
     return (
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(activity =>(
-                    <ActivityListItem key={activity.id} activity={activity}/>
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedAcivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color="teal">
+                        {group}
+                    </Header>
+                    <Segment>
+                        <Item.Group divided>
+                            {activities.map(activity => (
+                                <ActivityListItem key={activity.id} activity={activity} />
+                            ))}
+                        </Item.Group>
+                    </Segment>
+                </Fragment>
+            ))}
+        </>
+
     )
 })
