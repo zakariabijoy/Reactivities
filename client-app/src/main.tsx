984997a063ce9ddb409f62abbb9d8487df4d163b
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-17 16:37:21 +0600

    Routing: Installing React Router

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router}/>
     </StoreContext.Provider>
   </React.StrictMode>,
 )
