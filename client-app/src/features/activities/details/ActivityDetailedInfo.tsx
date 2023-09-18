8b5431f9c8b99be0599030a8cffedbc1c20a3d1c
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-19 02:36:09 +0600

    Styling the UI: Populating the detailed components

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,5 +1,46 @@
-export default function ActivityDetailedInfo(){
-    return(
-        <h1>Info</h1>
-    )
+import { observer } from 'mobx-react-lite';
+import {Segment, Grid, Icon} from 'semantic-ui-react'
+import {Activity} from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity
 }
+
+export default observer(function ActivityDetailedInfo({activity}: Props) {
+    return (
+        <Segment.Group>
+            <Segment attached='top'>
+                <Grid>
+                    <Grid.Column width={1}>
+                        <Icon size='large' color='teal' name='info'/>
+                    </Grid.Column>
+                    <Grid.Column width={15}>
+                        <p>{activity.description}</p>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+            <Segment attached>
+                <Grid verticalAlign='middle'>
+                    <Grid.Column width={1}>
+                        <Icon name='calendar' size='large' color='teal'/>
+                    </Grid.Column>
+                    <Grid.Column width={15}>
+            <span>
+              {activity.date}
+            </span>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+            <Segment attached>
+                <Grid verticalAlign='middle'>
+                    <Grid.Column width={1}>
+                        <Icon name='marker' size='large' color='teal'/>
+                    </Grid.Column>
+                    <Grid.Column width={11}>
+                        <span>{activity.venue}, {activity.city}</span>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+        </Segment.Group>
+    )
+})
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -24,8 +24,8 @@ export default observer(function ActivityDetails() {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityDetailedHeader/>
-                <ActivityDetailedInfo/>
+                <ActivityDetailedHeader activity={activity}/>
+                <ActivityDetailedInfo activity={activity}/>
                 <ActivityDetailedChat/>
             </Grid.Column>
             <Grid.Column width={6}>
