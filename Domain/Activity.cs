f6cdf3de95684588a9b1bd19fe674d43dd4740ba
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-24 02:13:02 +0600

    Error handling: Validation with data annotations

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,8 +1,12 @@
-﻿namespace Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
 
 public class Activity
 {
     public Guid Id { get; set; }
+
+    [Required]
     public string Title { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
