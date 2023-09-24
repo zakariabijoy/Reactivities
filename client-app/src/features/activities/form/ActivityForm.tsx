35bae6e716f74cf5f1bab59a147d132f53b0731e
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-25 02:40:04 +0600

    Forms:  Formik with less code

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
@@ -6,7 +6,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from 'uuid';
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 
 
 export default observer(function ActivityForm(){
@@ -48,18 +48,18 @@ export default observer(function ActivityForm(){
     return (
         <Segment clearing>
             <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
-                {({values: activity, handleChange, handleSubmit}) =>(
-                    <Form onSubmit={handleSubmit} autoComplete='off'>
-                    <Form.Input placeholder="Title" value={activity.title} name='title' onChange={handleChange}/>
-                    <Form.TextArea placeholder="Description"  value={activity.description} name='description' onChange={handleChange}/>
-                    <Form.Input placeholder="Category"  value={activity.category} name='category' onChange={handleChange}/>
-                    <Form.Input type="date" placeholder="Date"  value={activity.date} name='date' onChange={handleChange}/>
-                    <Form.Input placeholder="City"  value={activity.city} name='city' onChange={handleChange}/>
-                    <Form.Input placeholder="Venue"  value={activity.venue} name='venue' onChange={handleChange}/>
-                    <Button loading={loading} floated="right" positive type="submit" content='Submit'/>
-                    <Button as={Link} to='/activities' floated="right" type="button" content='Cancel'/>
-                </Form>
-            )}
+                {({handleSubmit}) =>(
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                        <Field placeholder="Title" name='title'/>
+                        <Field placeholder="Description" name='description'/>
+                        <Field placeholder="Category" name='category'/>
+                        <Field type="date" placeholder="Date" name='date'/>
+                        <Field placeholder="City" name='city'/>
+                        <Field placeholder="Venue" name='venue'/>
+                        <Button loading={loading} floated="right" positive type="submit" content='Submit'/>
+                        <Button as={Link} to='/activities' floated="right" type="button" content='Cancel'/>
+                    </Form>
+                )}
             </Formik>        
         </Segment>
     )
