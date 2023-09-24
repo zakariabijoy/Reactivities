d14b176e104217d911bd716e92a7176e5e727586
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-24 15:23:01 +0600

    Error handling: Preparing for setting up error handling in the client app 2

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,11 +3,13 @@ import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
   return (
     <>
+    <ToastContainer position='bottom-right' hideProgressBar theme='colored'/>
       {location.pathname === '/' ? <HomePage/> : (
         <>
           <NavBar/>
