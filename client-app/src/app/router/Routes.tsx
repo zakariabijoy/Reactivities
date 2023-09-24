8bcfb252cf068df903efbfe8c0c9623edd96f4d1
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-24 15:22:28 +0600

    Error handling: Preparing for setting up error handling in the client app

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,9 +1,9 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboad/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
 
 export const routes: RouteObject[] =[
     {
@@ -13,7 +13,8 @@ export const routes: RouteObject[] =[
             { path:'activities', element: <ActivityDashboard/>},
             { path:'activities/:id', element: <ActivityDetails/>},
             { path:'createActivity', element: <ActivityForm key='create'/>},  // have to use key if we want to use same component for diffrent route to reset state
-            { path:'manage/:id', element: <ActivityForm key='manage'/>}
+            { path:'manage/:id', element: <ActivityForm key='manage'/>},
+            { path:'errors', element: <TestErrors/>}
         ]
     },
 ]
---
+++ client-app/src/features/errors/TestError.tsx
@@ -0,0 +1,46 @@
+import {Button, Header, Segment} from "semantic-ui-react";
+import axios from 'axios';
+
+export default function TestErrors() {
+    const baseUrl = 'http://localhost:5010/api/'
+
+    function handleNotFound() {
+        axios.get(baseUrl + 'buggy/not-found').catch(err => console.log(err.response));
+    }
+
+    function handleBadRequest() {
+        axios.get(baseUrl + 'buggy/bad-request').catch(err => console.log(err.response));
+    }
+
+    function handleServerError() {
+        axios.get(baseUrl + 'buggy/server-error').catch(err => console.log(err.response));
+    }
+
+    function handleUnauthorised() {
+        axios.get(baseUrl + 'buggy/unauthorised').catch(err => console.log(err.response));
+    }
+
+    function handleBadGuid() {
+        axios.get(baseUrl + 'activities/notaguid').catch(err => console.log(err.response));
+    }
+
+    function handleValidationError() {
+        axios.post(baseUrl + 'activities', {}).catch(err => console.log(err.response));
+    }
+
+    return (
+        <>
+            <Header as='h1' content='Test Error component' />
+            <Segment>
+                <Button.Group widths='7'>
+                    <Button onClick={handleNotFound} content='Not Found' basic primary />
+                    <Button onClick={handleBadRequest} content='Bad Request' basic primary />
+                    <Button onClick={handleValidationError} content='Validation Error' basic primary />
+                    <Button onClick={handleServerError} content='Server Error' basic primary />
+                    <Button onClick={handleUnauthorised} content='Unauthorised' basic primary />
+                    <Button onClick={handleBadGuid} content='Bad Guid' basic primary />
+                </Button.Group>
+            </Segment>
+        </>
+    )
+}
