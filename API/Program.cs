5b01ee550a0a1b2f1bdc8e769e84cbac69ebb7de
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-24 15:10:08 +0600

    Error handling: Exception Handling Middleware -API

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -13,6 +14,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseSwagger();
 app.UseSwaggerUI();
