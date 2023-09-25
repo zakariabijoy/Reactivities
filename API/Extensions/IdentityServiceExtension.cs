886803788d72a8a19c9dd9c0080eea1de7429c5c
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-26 03:41:17 +0600

    ASPNETCore Identity: Configuring Identity in Program Class with identityservice extension method

---
+++ API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,19 @@
+using Domain;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtension
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>(opt =>{
+            opt.Password.RequireNonAlphanumeric = false;
+        })
+        .AddEntityFrameworkStores<DataContext>();
+
+        services.AddAuthentication();
+        
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 
 var app = builder.Build();
