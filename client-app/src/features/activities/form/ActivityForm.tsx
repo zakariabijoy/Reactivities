8cb71034a5e5c5919602656bac942515e44a9f27
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-26 02:47:31 +0600

    Forms: Hooking up the form submission to Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,12 @@
-import { Button, Segment } from "semantic-ui-react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { Button, Header, Segment } from "semantic-ui-react";
+import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
 import GenericTextInput from "../../../app/common/form/GenericTextInput";
 import GenericTextArea from "../../../app/common/form/GenericTextArea";
@@ -18,7 +18,7 @@ import GenericDateInput from "../../../app/common/form/GenericDateInput";
 export default observer(function ActivityForm(){
     
     const {activityStore} = useStore();
-    const {selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
+    const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
     const navigate = useNavigate();
 
@@ -36,7 +36,7 @@ export default observer(function ActivityForm(){
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The activity description is required'),
         category: Yup.string().required(),
-        date: Yup.string().required(),
+        date: Yup.string().required('date is required'),
         venue: Yup.string().required(),
         city: Yup.string().required(),
     });
@@ -45,29 +45,25 @@ export default observer(function ActivityForm(){
         if(id) loadActivity(id).then(activity => setActivity(activity!))
     },[id, loadActivity]);
     
-    // function handleSubmit(){
-    //     if(!activity.id){
-    //         activity.id = uuid();
-    //         createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     }else{
-    //         updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     }
-    // }
-
-    // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
-    //     const {name , value} = event.target;
-    //     setActivity({...activity, [name]:value})
-    // }
+    function handleFormSubmit(activity: Activity){
+        if(!activity.id){
+            activity.id = uuid();
+            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }else{
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }
+    }
 
     if(loadingInitial) return <LoadingComponent content="Loading Activity ..."/>
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color="teal" />
             <Formik 
                 validationSchema={validationSchema}
                 enableReinitialize 
                 initialValues={activity} 
-                onSubmit={values => console.log(values)}>
-                {({handleSubmit}) =>(
+                onSubmit={values => handleFormSubmit(values)}>
+                {({handleSubmit, isValid, isSubmitting, dirty}) =>(
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <GenericTextInput name="title" placeholder="Title"/>    
                         <GenericTextArea rows={3} placeholder="Description" name='description'/>
@@ -79,9 +75,17 @@ export default observer(function ActivityForm(){
                             timeCaption="time"
                             dateFormat='MMMM d, yyyy h:mm aa'
                         />
+                        <Header content='Location Details' sub color="teal" />
                         <GenericTextInput placeholder="City" name='city'/>
                         <GenericTextInput placeholder="Venue" name='venue'/>
-                        <Button loading={loading} floated="right" positive type="submit" content='Submit'/>
+                        <Button 
+                            disabled={isSubmitting || !dirty || !isValid}
+                            loading={loading} 
+                            floated="right" 
+                            positive 
+                            type="submit" 
+                            content='Submit'
+                        />
                         <Button as={Link} to='/activities' floated="right" type="button" content='Cancel'/>
                     </Form>
                 )}
