f91adca56c404cb159dbd3fff6b2d3d241eaa088
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-26 03:51:38 +0600

    ASPNETCore Identity: Adding seed users

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -34,8 +36,9 @@ var services =scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
