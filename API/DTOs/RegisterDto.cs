52b2023d200c2f2e78fcd08eff55f9497e298912
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-08 18:34:23 +0600

    Identity: Validating the registration of users

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs;
 
 public class RegisterDto
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Required]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage ="Password must be complex")]
     public string Password { get; set; }
+
+    [Required]
     public string DisplayName { get; set; }
+
+    [Required]
     public string Username { get; set; }
 }
