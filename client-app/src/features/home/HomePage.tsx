8a09e65c4651cd48db4cab8e3a1d714317460434
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-15 00:45:02 +0600

    Client side login and registration: Updating the home page and nav bar

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function HomePage(){
+export default observer(function HomePage(){
+    const {userStore}  = useStore();
+
     return(
         <Segment inverted textAlign="center" vertical className="masthead">
             <Container text>
@@ -9,11 +13,20 @@ export default function HomePage(){
                     <Image size="massive" src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
                     Reactivities
                 </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities'/>
-                <Button as={Link} to='/login' size="huge" inverted>
-                    Login!
-                </Button>
+                {userStore.isLoggedIn? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities'/>
+                        <Button as={Link} to='/activities' size="huge" inverted>
+                            Go to Activities!
+                        </Button>
+                    </>
+                    
+                ): (
+                    <Button as={Link} to='/login' size="huge" inverted>
+                        Login!
+                    </Button>
+                )}        
             </Container>
         </Segment>
     )
-}
+})
