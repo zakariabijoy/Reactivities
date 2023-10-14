20a154a220fb84d0729133486340f7331416219f
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-14 16:55:09 +0600

    Client side login and registration: Setting the token upon login

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,6 +3,8 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore{
     error: ServerError | null = null;
+    token: string | null | undefined = null;
+    appLoaded = false;
 
     constructor(){
         makeAutoObservable(this);
@@ -10,5 +12,14 @@ export default class CommonStore{
 
     setServerError(error: ServerError){
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        if(token) localStorage.setItem('jwt', token);
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 }
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
+import { store } from "./store";
+import { router } from "../router/Routes";
 
 export default class UserStore{
     user: User | null = null;
@@ -15,6 +17,15 @@ export default class UserStore{
 
     login = async (creds: UserFormValues) =>{
         const user = await agent.Account.login(creds);
-        console.log(user);
+        store.commonStore.setToken(user.token);
+        runInAction(() => this.user = user);
+        router.navigate('/activities');
+    }
+
+    logout = () =>{
+        store.commonStore.setToken(null);
+        localStorage.removeItem('jwt');
+        this.user = null;
+        router.navigate('/');
     }
 }
