695b874126a68be1f2785f69bb93a6df2e884e78
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-14 15:26:28 +0600

    Client side login and registration: Displaying errors in the form

--- client-app/src/app/users/LoginForm.tsx
+++ client-app/src/app/users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Formik } from "formik";
+import { ErrorMessage, Formik } from "formik";
 import GenericTextInput from "../common/form/GenericTextInput";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Label } from "semantic-ui-react";
 import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -9,13 +9,19 @@ export default observer (function LoginForm()
     const {userStore} = useStore();
     return (
         <Formik
-            initialValues={{email:'', password:''}}
-            onSubmit={values => userStore.login(values)}
+            initialValues={{email:'', password:'', error: null}}
+            onSubmit={(values, {setErrors}) => userStore.login(values).catch(err => 
+                setErrors({error: 'Invalid email or password' }) )}
         >
-            {({handleSubmit, isSubmitting}) => (
+            {({handleSubmit, isSubmitting, errors}) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                     <GenericTextInput placeholder="Email" name='email'/>
                     <GenericTextInput placeholder="Password" name='password' type="password"/>
+                    <ErrorMessage
+                    name="error"
+                    render={() =>
+                     <Label style={{marginBottom: 10}} basic color="red" content={errors.error}/>}
+                     />
                     <Button loading={isSubmitting} positive content='Login' type="submit" fluid/>
                 </Form>
             )}
