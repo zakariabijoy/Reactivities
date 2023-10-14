fe412cc8f4f16d1ebc2814cf753bf8413bcc94ce
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-14 14:37:51 +0600

    Client side login and registration: Creating the interfaces and methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../models/user";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) =>{
@@ -69,8 +70,15 @@ const Activities = {
     delete: (id: string) => request.del<void>(`/activities/${id}`)
 }
 
+const Account ={
+    current: () => request.get<User>('/account'),
+    login: (user: UserFormValues) => request.post<User>('/account/login', user),
+    register: (user: UserFormValues) => request.post<User>('/account/register', user),
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
