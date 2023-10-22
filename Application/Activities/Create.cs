c3d299edc9267749feea7ffb692a4278fc85b863
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-22 12:24:58 +0600

    Ef Relationship: Updating the create activity handler

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,9 @@
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities;
@@ -24,14 +26,27 @@ public class Create
     public class Handler : IRequestHandler<Command, Result<Unit>>
     {
         private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(DataContext context)
+        public Handler(DataContext context, IUserAccessor userAccessor)
         {
+            _userAccessor = userAccessor;
             _context = context;
         }
 
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var user =  await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+            var attendee = new ActivityAttendee
+            {
+                AppUser = user,
+                Activity = request.Activity,
+                IsHost = true
+            };
+
+            request.Activity.Attendees.Add(attendee);
+            
             _context.Activities.Add(request.Activity);
 
             var result = await _context.SaveChangesAsync() > 0;
