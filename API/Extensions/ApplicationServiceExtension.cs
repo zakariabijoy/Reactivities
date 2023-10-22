58f278b7463252f1cad34d172d7e1cf5012a3bdd
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-22 11:49:25 +0600

    Ef Relationship: Adding an infrastructure project

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -1,7 +1,9 @@
 using Application.Activities;
 using Application.Core;
+using Application.Interfaces;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -29,6 +31,8 @@ public static class ApplicationServiceExtension
         services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         services.AddFluentValidationAutoValidation();
         services.AddValidatorsFromAssemblyContaining<Create>();
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUserAccessor, UserAccessor>();
 
         return services;
     }
---
+++ Application/Interfaces/IUserAccessor.cs
@@ -0,0 +1,6 @@
+namespace Application.Interfaces;
+
+public interface IUserAccessor
+{
+    string GetUsername();
+}
