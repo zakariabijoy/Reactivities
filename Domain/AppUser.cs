53e3eb3a9f9b0404c8878c3a14b7e7c42a5e1282
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-28 14:22:21 +0600

    API Image Upload: Adding the Photo entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -7,4 +7,5 @@ public class AppUser : IdentityUser
     public string DisplayName { get; set; }
     public string Bio { get; set; }
     public ICollection <ActivityAttendee> Activities{ get; set; }
+    public ICollection<Photo> Photos { get; set; }
 }
---
+++ Persistence/Migrations/20231028081952_PhotoEntityAdded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    IsMain = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AppUserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photos",
+                column: "AppUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
