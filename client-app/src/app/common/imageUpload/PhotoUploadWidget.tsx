35bead18288af92244aeb864bb80b1c24334470e
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-29 03:42:39 +0600

    Client Image Upload:   Adding a react croppe

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,25 +1,49 @@
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Button, Grid, Header, Image } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
-export default function PhotoUploadWidget(){
+export default function PhotoUploadWidget() {
     const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    function onCrop() {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob(blob => console.log(blob));
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            files.forEach((file: any) => URL.revokeObjectURL(file.preview))
+        }
+    }, [files]);
+
     return (
         <Grid>
             <Grid.Column width={4}>
-                <Header sub color="teal" content='Step 1 - Add Photo'/>
-                <PhotoWidgetDropzone setFiles={setFiles}/>
+                <Header sub color="teal" content='Step 1 - Add Photo' />
+                <PhotoWidgetDropzone setFiles={setFiles} />
             </Grid.Column>
-            <Grid.Column width={1}/>
+            <Grid.Column width={1} />
             <Grid.Column width={4}>
-                <Header sub color="teal" content='Step 2 - Resize Image'/>
+                <Header sub color="teal" content='Step 2 - Resize Image' />
                 {files && files.length > 0 && (
-                    <Image src={files[0].preview}/>
+                    <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
                 )}
             </Grid.Column>
-            <Grid.Column width={1}/>
+            <Grid.Column width={1} />
             <Grid.Column width={4}>
-                <Header sub color="teal" content='Step 3 - preview & Upload'/>
+                <Header sub color="teal" content='Step 3 - preview & Upload' />
+                {files && files.length > 0 &&
+                    <>
+                        <div className="img-preview" style={{ minHeight: 200, overflow: 'hidden' }} />
+                        <Button.Group widths={2}>
+                            <Button  onClick={onCrop} positive icon='check' />
+                            <Button  onClick={() => setFiles([])} icon='close' />
+                        </Button.Group>
+                    </>
+                }
             </Grid.Column>
         </Grid>
     )
