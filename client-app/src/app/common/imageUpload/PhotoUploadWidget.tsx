e4d809ba141c6bbf348bb099ac8862540fb4718b
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-29 03:07:42 +0600

    Client Image Upload:  Adding a Dropzone

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,10 +1,12 @@
 import { Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget(){
     return (
         <Grid>
             <Grid.Column width={4}>
                 <Header sub color="teal" content='Step 1 - Add Photo'/>
+                <PhotoWidgetDropzone/>
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
---
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,20 @@
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+export default function PhotoWidgetDropzone() {
+  const onDrop = useCallback((acceptedFiles:any) => {
+   console.log(acceptedFiles);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
