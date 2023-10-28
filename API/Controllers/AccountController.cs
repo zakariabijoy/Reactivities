7d8a6e669afc28923d32c1ff566023818a574804
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-28 18:25:04 +0600

    API Image Upload: Returning an attendee DTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ public class AccountController : ControllerBase
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user =  await _userManager.FindByEmailAsync(loginDto.Email);
+        var user =  await _userManager.Users.Include(u => u.Photos).FirstOrDefaultAsync(u => u.Email == loginDto.Email);
 
         if(user is null) return Unauthorized();
 
@@ -77,7 +77,7 @@ public class AccountController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<UserDto>> GetCurrentUser()
     {
-        var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+        var user = await _userManager.Users.Include(u => u.Photos).FirstOrDefaultAsync(u => u.Email == User.FindFirstValue(ClaimTypes.Email));
 
         return CreateUserObject(user);
     }
@@ -87,7 +87,7 @@ public class AccountController : ControllerBase
         return new UserDto
         {
             DisplayName = user.DisplayName,
-            Image = null,
+            Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
             Token = _tokenService.CreateToken(user),
             Username = user.UserName
         };
--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -13,5 +13,5 @@ public class ActivityDto
     public string Venue { get; set; }
     public string HostUsername { get; set; }
     public bool IsCancelled { get; set; }
-    public ICollection<Profile> Attendees { get; set; }
+    public ICollection<AttendeeDto> Attendees { get; set; }
 }
