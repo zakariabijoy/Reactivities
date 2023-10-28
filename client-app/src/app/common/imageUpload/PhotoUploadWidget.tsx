04cd0d11373fdfc79ca6b7c8fc0c29ac4fb98f98
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-29 03:22:05 +0600

    Client Image Upload:  Styling the Dropzone

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,16 +1,21 @@
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import { useState } from "react";
 
 export default function PhotoUploadWidget(){
+    const [files, setFiles] = useState<any>([]);
     return (
         <Grid>
             <Grid.Column width={4}>
                 <Header sub color="teal" content='Step 1 - Add Photo'/>
-                <PhotoWidgetDropzone/>
+                <PhotoWidgetDropzone setFiles={setFiles}/>
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
                 <Header sub color="teal" content='Step 2 - Resize Image'/>
+                {files && files.length > 0 && (
+                    <Image src={files[0].preview}/>
+                )}
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
--- client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -1,20 +1,38 @@
-import {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Header, Icon } from 'semantic-ui-react';
 
-export default function PhotoWidgetDropzone() {
-  const onDrop = useCallback((acceptedFiles:any) => {
-   console.log(acceptedFiles);
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+interface Props {
+    setFiles: (files: any) => void;
+}
 
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
+export default function PhotoWidgetDropzone({ setFiles }: Props) {
+    const dzStyles = {
+        border: 'dashed 3px #eee',
+        borderColor: '#eee',
+        borderRadius: '5px',
+        paddingTop: '30px',
+        textAlign: 'center' as 'center',
+        height: '200px'
+    }
+
+    const dzActive = {
+        borderColor: 'green',
+    }
+
+    const onDrop = useCallback((acceptedFiles: any) => {
+        setFiles(acceptedFiles.map((file: any) => Object.assign(file,{
+            preview: URL.createObjectURL(file)
+        })))
+    }, [setFiles])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+    return (
+        <div {...getRootProps()} style={isDragActive ? {...dzStyles, ...dzActive} : dzStyles}>
+            <input {...getInputProps()} />
+            <Icon name='upload' size='huge'/>
+            <Header content='Drop image here'/>
+        </div>
+    )
 }
