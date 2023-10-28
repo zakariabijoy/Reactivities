b48aee09297d5eaccedbbe80e1fd95fb6f1ee479
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-28 18:04:42 +0600

    API Image Upload: Updating the mapping configuration

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,7 +15,8 @@ public class MappingProfiles : Profile
         CreateMap<ActivityAttendee, Profiles.Profile>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName)) 
             .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName)) 
-            .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio)); 
-    }
-    
+            .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+        CreateMap<AppUser, Profiles.Profile>()
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)); 
+    }  
 }
