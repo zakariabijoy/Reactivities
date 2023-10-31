f8de8c153242db4bb026014c93dbe0c92cd14623
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-31 19:40:03 +0600

    SignalR: Adding a comment DTO and mapping

---
+++ Application/Comments/CommentDto.cs
@@ -0,0 +1,10 @@
+namespace Application.Comments;
+
+public class CommentDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string Username { get; set; }
+    public string DisplayName { get; set; }
+    public string Image { get; set; }
+}
--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,4 +1,5 @@
 using Application.Activities;
+using Application.Comments;
 using AutoMapper;
 using Domain;
 
@@ -12,12 +13,19 @@ public class MappingProfiles : Profile
         CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostUsername,
             o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
         CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName)) 
             .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName)) 
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
             .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+
         CreateMap<AppUser, Profiles.Profile>()
             .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)); 
+
+        CreateMap<Comment, CommentDto>()
+            .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName)) 
+            .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName)) 
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
     }  
 }
