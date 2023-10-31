1427a5a62030ebe54b66f2671139fd78a9b2adb5
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-10-31 23:25:01 +0600

    SignalR: Adding a list handler

---
+++ Application/Comments/List.cs
@@ -0,0 +1,39 @@
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comments;
+
+public class List
+{
+    public class Query : IRequest<Result<List<CommentDto>>>
+    {
+        public Guid ActivityId { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+
+        public Handler(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<CommentDto>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var comments = await _context.Comments
+                .Where(x => x.Activity.Id == request.ActivityId)
+                .OrderBy(x => x.CreatedAt)
+                .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return Result<List<CommentDto>>.Success(comments);
+        }
+    }
+}
