e42b6e6d66e380481a2cea158f2d023c3c0a4109
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-11-01 23:47:40 +0600

    SignalR: Authenticating to SignalR

--- API/Extensions/IdentityServiceExtension.cs
+++ API/Extensions/IdentityServiceExtension.cs
@@ -30,6 +30,20 @@ public static class IdentityServiceExtension
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if(!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+                        {
+                            context.Token = accessToken;
+                        }
+                        
+                        return Task.CompletedTask;
+                    }
+                };
             });
         
         services.AddAuthorization(opt => opt.AddPolicy("IsActivityHost", policy =>
