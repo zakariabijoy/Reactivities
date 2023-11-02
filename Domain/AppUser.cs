1ad6e96411fa3d3dbc11395aaad2f2a1d832c52b
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-11-03 01:25:32 +0600

    Followers/ Following Feature: Adding a join entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -8,4 +8,6 @@ public class AppUser : IdentityUser
     public string Bio { get; set; }
     public ICollection <ActivityAttendee> Activities{ get; set; }
     public ICollection<Photo> Photos { get; set; }
+    public ICollection<UserFollowing> Followings { get; set; }
+    public ICollection<UserFollowing> Followers { get; set; }
 }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -14,6 +14,7 @@ public class DataContext : IdentityDbContext<AppUser>
     public DbSet<ActivityAttendee>  ActivityAttendees { get; set; }
     public DbSet<Photo>  Photos { get; set; }
     public DbSet<Comment>  Comments { get; set; }
+    public DbSet<UserFollowing>  UserFollowings { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -35,5 +36,19 @@ public class DataContext : IdentityDbContext<AppUser>
             .HasOne(c => c.Activity)
             .WithMany(a => a.Comments)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<UserFollowing>(b => {
+            b.HasKey(k => new {k.ObserverId, k.TargetId});
+
+            b.HasOne(o => o.Observer)
+                .WithMany(f => f.Followings)
+                .HasForeignKey(o => o.ObserverId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            b.HasOne(o => o.Target)
+                .WithMany(f => f.Followers)
+                .HasForeignKey(o => o.TargetId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
