a4d083ebccec244378d661346a0622d9105bb93c
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-17 00:35:46 +0600

    Followers/Following Features: Return a list of followers

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -7,4 +7,8 @@ public class FollowController : BaseApiController
 {
     [HttpPost("{username}")]
     public async Task<IActionResult> Follow(string username) => HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetFollowings(string username, string predicate) => 
+                                            HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
 }
