cb8d49031548d47d4edf481b57aeb886b97171cb
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-20 01:20:59 +0600

    Paging, Sorting and Filtering: Adding placeholders

--- client-app/src/features/activities/dashboad/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboad/ActivityDashboard.tsx
@@ -3,10 +3,10 @@ import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from "./ActivityFilter";
 import { PagingParams } from "../../../app/models/pagination";
 import InfiniteScroll from "react-infinite-scroller";
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceHolder";
 
 
 export default observer(function ActivityDashboard(){
@@ -25,19 +25,24 @@ export default observer(function ActivityDashboard(){
       if(activityRegistry.size <= 1) loadActivities();
     }, [loadActivities, activityRegistry.size]);
   
-    if(activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...'/>
-
     return(
         <Grid>
             <Grid.Column width='10'>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={handleGetNext}
-                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-                    initialLoad={false}
-                >
-                    <ActivityList/>
-                </InfiniteScroll>
+                {activityStore.loadingInitial && activityRegistry.size === 0  && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder/>
+                        <ActivityListItemPlaceholder/>
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        initialLoad={false}
+                    >
+                        <ActivityList/>
+                    </InfiniteScroll>
+                )}         
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters/>
---
+++ client-app/src/features/activities/dashboad/ActivityListItemPlaceHolder.tsx
@@ -0,0 +1,33 @@
+import { Segment, Button, Placeholder } from 'semantic-ui-react';
+
+export default function ActivityListItemPlaceholder() {
+    return (
+        <>
+            <Placeholder fluid style={{ marginTop: 25 }}>
+                <Segment.Group>
+                    <Segment style={{ minHeight: 110 }}>
+                        <Placeholder>
+                            <Placeholder.Header image>
+                                <Placeholder.Line />
+                                <Placeholder.Line />
+                            </Placeholder.Header>
+                            <Placeholder.Paragraph>
+                                <Placeholder.Line />
+                            </Placeholder.Paragraph>
+                        </Placeholder>
+                    </Segment>
+                    <Segment>
+                        <Placeholder>
+                            <Placeholder.Line />
+                            <Placeholder.Line />
+                        </Placeholder>
+                    </Segment>
+                    <Segment secondary style={{ minHeight: 70 }} />
+                    <Segment clearing>
+                        <Button disabled color='blue' floated='right' content='View' />
+                    </Segment>
+                </Segment.Group>
+            </Placeholder>
+        </>
+    );
+};
