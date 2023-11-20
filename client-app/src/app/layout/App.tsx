b644a4ed9512d34424c2d6aa10113462c4ff427f
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-20 23:58:56 +0600

    Finishing touches and publishing: Adding Scroll to top for our routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import {Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
 import { useStore } from '../stores/store';
@@ -25,6 +25,7 @@ function App() {
   
   return (
     <>
+    <ScrollRestoration/>
     <ModalContainer />
     <ToastContainer position='bottom-right' hideProgressBar theme='colored'/>
       {location.pathname === '/' ? <HomePage/> : (
