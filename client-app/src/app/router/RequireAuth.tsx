62f5f85de646f8f8ce30460fe960782f32bdffe1
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-21 00:10:36 +0600

    Finishing touches and publishing: Adding private routes

---
+++ client-app/src/app/router/RequireAuth.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useStore } from "../stores/store";
+
+export default function RequireAuth(){
+    const {userStore:{isLoggedIn}} = useStore();
+    const location = useLocation();
+
+    if(!isLoggedIn){
+        return <Navigate to={'/'} state={{from: location}}/>
+    }
+    return <Outlet/>
+}
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -6,21 +6,22 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotForund";
 import ServerError from "../../features/errors/ServerError";
-import LoginForm from "../users/LoginForm";
 import ProfilePage from "../../features/Profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] =[
     {
         path: '/',
         element: <App/>,
         children:[
-            { path:'activities', element: <ActivityDashboard/>},
-            { path:'activities/:id', element: <ActivityDetails/>},
-            { path:'createActivity', element: <ActivityForm key='create'/>},  // have to use key if we want to use same component for diffrent route to reset state
-            { path:'manage/:id', element: <ActivityForm key='manage'/>},
-            { path:'profiles/:username', element: <ProfilePage/>},
-            { path:'login', element: <LoginForm/>},
-            { path:'errors', element: <TestErrors/>},
+            {element: <RequireAuth/>, children:[
+                { path:'activities', element: <ActivityDashboard/>},
+                { path:'activities/:id', element: <ActivityDetails/>},
+                { path:'createActivity', element: <ActivityForm key='create'/>},  // have to use key if we want to use same component for diffrent route to reset state
+                { path:'manage/:id', element: <ActivityForm key='manage'/>},
+                { path:'profiles/:username', element: <ProfilePage/>},
+                { path:'errors', element: <TestErrors/>},
+            ]},
             { path:'not-found', element: <NotFound/>},
             { path:'server-error', element: <ServerError/>},
             { path:'*', element: <Navigate replace to='/not-found'/>}
