f82303f4550e3cb5617301ceefe62fa1ea7b41ec
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-21 01:16:41 +0600

    Finishing touches and publishing: Running the client app on the dotnet Kestrel server

--- API/Program.cs
+++ API/Program.cs
@@ -35,8 +35,12 @@ app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
+app.MapFallbackToController("Index","Fallback");
 
 using var scope = app.Services.CreateScope();
 var services =scope.ServiceProvider;
