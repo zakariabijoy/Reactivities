6bda8e03961b0a635f9b068e17e84963f0d9633d
Md Zakaria Masud <zakaria.masud@surbanajurong.com>
2023-11-24 01:03:45 +0600

    Finishing touches and publishing: Deploy to flyio - creating the config variables and secrets

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -18,9 +18,44 @@ public static class ApplicationServiceExtension
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
-        services.AddDbContext<DataContext>(opt => {
-            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        services.AddDbContext<DataContext>(options =>
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            string connStr;
+
+            // Depending on if in development or production, use either FlyIO
+            // connection string, or development connection string from env var.
+            if (env == "Development")
+            {
+                // Use connection string from file.
+                connStr = config.GetConnectionString("DefaultConnection");
+            }
+            else
+            {
+                // Use connection string provided at runtime by FlyIO.
+                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl.Split("@")[0];
+                var pgHostPortDb = connUrl.Split("@")[1];
+                var pgHostPort = pgHostPortDb.Split("/")[0];
+                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgUser = pgUserPass.Split(":")[0];
+                var pgPass = pgUserPass.Split(":")[1];
+                var pgHost = pgHostPort.Split(":")[0];
+                var pgPort = pgHostPort.Split(":")[1];
+                var updatedHost = pgHost.Replace("flycast", "internal");
+
+                connStr = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+            }
+
+            // Whether the connection string came from the local development configuration file
+            // or from the environment variable from FlyIO, use it to set up your DbContext.
+            options.UseNpgsql(connStr);
         });
+
 
         services.AddCors(opt =>{
         opt.AddPolicy("CorsPolicy", policy =>{
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build-env
 WORKDIR /app
+EXPOSE 8080
 
 # copy  .csproj and restore as distinct kayers
 COPY "Reactivities.sln" "Reactivities.sln"
