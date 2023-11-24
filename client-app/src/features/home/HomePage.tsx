e4d976114952563a555e5d6b33b1e00053d9268d
zakariabijoy <zakaria.bijoy@live.com>
2023-11-25 00:47:38 +0600

    Identity Cookbook: Adding the Facebook JS SDK

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button, Divider } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoginForm from "../../app/users/LoginForm";
 import RegisterForm from "../../app/users/RegisterForm";
+import FacebookLogin, { FailResponse, SuccessResponse } from "@greatsumini/react-facebook-login";
 
 export default observer(function HomePage(){
     const {userStore, modalStore}  = useStore();
@@ -32,6 +33,17 @@ export default observer(function HomePage(){
                     <Button onClick={() => modalStore.openModal(<RegisterForm/>)} size="huge" inverted>
                         Register
                     </Button>
+                    <Divider horizontal inverted>Or</Divider>
+                    <FacebookLogin
+                        appId="859222439029599"
+                        onSuccess={(response: SuccessResponse) => {
+                            console.log("Login Success!", response);
+                        }}
+                        onFail={(response: FailResponse) => {
+                            console.log("Login Failed!", response);
+                        }}
+                        className="ui button facebook huge inverted"
+                    />
                     </>
                 )}        
             </Container>
--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,8 @@ export default defineConfig({
     outDir:'../API/wwwroot'
   },
   server:{
-    port: 3000
+    port: 3000,
+    https: true
   },
-  plugins: [react()],
+  plugins: [react(), mkcert()],
 })
